74f70112e894df26f91a5ec50419c25065e531c9
Nassir Alkourdi <nassir-al@hotmail.com>
2022-09-22 13:07:31 +0200

    create toDoList

---
+++ src/components/toList.js
@@ -0,0 +1,21 @@
+import TodoItem from './todoitem';
+
+const TodoList = ({list, title, onDeleteTodo, onToggleTodo}) => {
+	return (
+		<div className="TodoListContainer">
+			<h2>{title}</h2>
+
+			{list.map(todo => (
+				<TodoItem
+					key={todo.id}
+					id={todo.id}
+					done={todo.done}
+					title={todo.title}
+					onDeleteTodo={onDeleteTodo}
+					onToggleTodo={onToggleTodo}
+				/>
+			))}
+		</div>
+	);
+};
+export default TodoList;
